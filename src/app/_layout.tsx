753cd1bb84f43627ad7856f8f47a0baf3e84e531
Masood <masoodacheampong@gmail.com>
2026-07-03 10:01:24 +0000

    fix: android startup crash

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -12,7 +12,6 @@ import {
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-import "../../global.css";
 
 void SplashScreen.preventAutoHideAsync();
 
